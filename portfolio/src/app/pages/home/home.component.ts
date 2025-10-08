3dfe15e5687fdcb40b5fceb4e63847528cf5314b
mohammed amaan <amaanvisiontech@gmail.com>
2025-10-09 00:03:59 +0530

    fix errors

--- portfolio/src/app/pages/home/home.component.ts
+++ portfolio/src/app/pages/home/home.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ThemeService } from '../../core/services/theme.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  theme = 'light';
+  title = 'Full Stack Developer';
+  name = 'Mohammed Amaan';
 
+  constructor(private themeService: ThemeService) {
+    this.themeService.theme$.subscribe(t => this.theme = t);
+  }
 }
