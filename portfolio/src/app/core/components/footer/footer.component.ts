c067bea6a564b7a556d8bd45060744b219255f70
mohammed amaan <amaanvisiontech@gmail.com>
2025-10-10 21:04:19 +0530

    update bug fixes

--- portfolio/src/app/core/components/footer/footer.component.ts
+++ portfolio/src/app/core/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+ theme = 'light';
+  year = new Date().getFullYear();
+  constructor(private themeService: ThemeService) {
+    this.themeService.theme$.subscribe(t => (this.theme = t));
+  }
 }
--- portfolio/src/app/pages/contact/contact.component.ts
+++ portfolio/src/app/pages/contact/contact.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../../core/services/theme.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  theme = 'light';
 
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  constructor(private themeService: ThemeService) {
+    this.themeService.theme$.subscribe((t) => (this.theme = t));
+  }
+
+  onSubmit() {
+    if (
+      this.contactData.name &&
+      this.contactData.email &&
+      this.contactData.message
+    ) {
+      alert(`Thank you, ${this.contactData.name}! Your message has been sent.`);
+      this.contactData = { name: '', email: '', message: '' };
+    } else {
+      alert('Please fill all fields.');
+    }
+  }
 }
