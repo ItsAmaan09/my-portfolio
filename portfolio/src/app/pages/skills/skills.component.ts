a261c72b2fe2f8ddf88f251fada71bfa3b9febd6
mohammed amaan <amaanvisiontech@gmail.com>
2025-10-10 20:50:15 +0530

    skills page update

--- portfolio/src/app/pages/skills/skills.component.ts
+++ portfolio/src/app/pages/skills/skills.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../../core/services/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
+  skills = [
+    { name: '.Net Core', level: 90 },
+    { name: 'Angular', level: 90 },
+    { name: 'Node.js', level: 85 },
+    { name: 'MS SQL', level: 80 },
+    { name: 'Bootstrap', level: 95 },
+    { name: 'AWS', level: 70 },
+  ];
 
+  theme = 'light';
+  constructor(private themeService: ThemeService) {
+    this.themeService.theme$.subscribe((t) => (this.theme = t));
+  }
 }
