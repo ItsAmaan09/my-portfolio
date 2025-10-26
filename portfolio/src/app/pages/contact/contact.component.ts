cb94f18e034576aa6f08e94d7e902d559ddf23b8
mohammed amaan <amaanvisiontech@gmail.com>
2025-10-26 20:49:14 +0530

    customize contace page

--- portfolio/src/app/pages/contact/contact.component.ts
+++ portfolio/src/app/pages/contact/contact.component.ts
@@ -22,35 +22,35 @@ export class ContactComponent {
     this.themeService.theme$.subscribe((t) => (this.theme = t));
   }
 
-  async onSubmit(form: NgForm) {
-    // if (form.invalid) {
-    //   form.control.markAllAsTouched(); // show errors
-    //   return;
-    // }
+  // async onSubmit(form: NgForm) {
+  //   // if (form.invalid) {
+  //   //   form.control.markAllAsTouched(); // show errors
+  //   //   return;
+  //   // }
 
-    const payload = {
-      access_key: 'b67d4ce7-58be-49b0-b2bd-d0168596efe5', // Replace with Web3Forms access key
-      name: this.contactData.name,
-      email: this.contactData.email,
-      message: this.contactData.message,
-    };
+  //   const payload = {
+  //     access_key: 'b67d4ce7-58be-49b0-b2bd-d0168596efe5', // Replace with Web3Forms access key
+  //     name: this.contactData.name,
+  //     email: this.contactData.email,
+  //     message: this.contactData.message,
+  //   };
 
-    try {
-      const res = await fetch('https://api.web3forms.com/submit', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(payload),
-      });
+  //   try {
+  //     const res = await fetch('https://api.web3forms.com/submit', {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify(payload),
+  //     });
 
-      if (res.ok) {
-        alert('✅ Message sent successfully!');
-        this.contactData = { name: '', email: '', message: '' };
-      } else {
-        alert('❌ Failed to send message, try again later.');
-      }
-    } catch (error) {
-      console.error(error);
-      alert('❌ Error sending message!');
-    }
-  }
+  //     if (res.ok) {
+  //       alert('✅ Message sent successfully!');
+  //       this.contactData = { name: '', email: '', message: '' };
+  //     } else {
+  //       alert('❌ Failed to send message, try again later.');
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //     alert('❌ Error sending message!');
+  //   }
+  // }
 }
